95e8c0089301cdbd7cf8ac050fe85f82fbc09752
saurav109 <scientistboy109@gmail.com>
2020-10-21 06:11:29 +0600

    first

---
+++ src/main/java/com/example/ironman/flowerdetection/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.ironman.flowerdetection;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final int INPUT_SIZE = 224;
+    private static final int IMAGE_MEAN = 117;
+    private static final float IMAGE_STD = 1;
+    private static final String INPUT_NAME = "input";
+    private static final String OUTPUT_NAME = "output";
+    private static final String MODEL_FILE = "file:///android_asset/og.pb";
+    private static final String LABEL_FILE =
+            "file:///android_asset/rl.txt";
+
+    private Classifier classifier;
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView =findViewById(R.id.result);
+    }
+
+
+
+    void detect(){
+        classifier =
+                TensorFlowImageClassifier.create(
+                        getAssets(),
+                        MODEL_FILE,
+                        LABEL_FILE,
+                        INPUT_SIZE,
+                        IMAGE_MEAN,
+                        IMAGE_STD,
+                        INPUT_NAME,
+                        OUTPUT_NAME);
+
+        Bitmap image = Bitmap.createBitmap(224, 224, Bitmap.Config.ARGB_8888);
+        final List<Classifier.Recognition> results = classifier.recognizeImage(image);
+
+        textView.setText(results.get(0).getTitle());
+
+
+    }
+
+    public static Bitmap getBitmapFromAsset(Context context, String filePath) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream istr;
+        Bitmap bitmap = null;
+        try {
+            istr = assetManager.open(filePath);
+            bitmap = BitmapFactory.decodeStream(istr);
+        } catch (IOException e) {
+            // handle exception
+        }
+
+        return bitmap;
+    }
+
+
+    public void startDetect(View view) {
+        detect();
+    }
+}
